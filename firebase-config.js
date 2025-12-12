499f270a3b5fbe8337f8cc539e2ee31f50bb6340
nw-com <nwcom.eason@gmail.com>
2025-12-12 17:17:41 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBa3EfodIJk4hZgH0zEHTlEGqSxncpkIbs",
-  authDomain: "nw-checkin-all-2026.firebaseapp.com",
-  projectId: "nw-checkin-all-2026",
-  storageBucket: "nw-checkin-all-2026.firebasestorage.app",
-  messagingSenderId: "840454751760",
-  appId: "1:840454751760:web:58bbfa83cb146f4fc7ac95",
-  measurementId: "G-NQ0W45T9CK"
-};
-
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyAmll4i3RPI_j-J7qWPkZIBYPoWEFqL9os",
+  authDomain: "nw-finance-2026.firebaseapp.com",
+  projectId: "nw-finance-2026",
+  storageBucket: "nw-finance-2026.firebasestorage.app",
+  messagingSenderId: "234539651970",
+  appId: "1:234539651970:web:1d14d3778831109634359b",
+  measurementId: "G-VV8QRLHKC9"
+});
+window.__force_demo_mode = false;
